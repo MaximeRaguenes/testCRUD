f70a21dc0245ea56f06d39be65ae0171f4d7c326
Maxime <maxime.raguenes2@gmail.com>
2018-11-02 02:02:50 +0100

    debug add

--- app/Animal.php
+++ app/Animal.php
@@ -7,8 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Animal extends Model
 { 
     protected $fillable = ['type', 'name', 'description'];
-
-    public function __construct(){
-        
-    }
 }
